711e7dc945782702c8355c23e0bb1c6f82ba770b
shashitha2002 <130375872+shashitha2002@users.noreply.github.com>
2025-05-03 15:10:17 +0530

    fix login import name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {Route,Routes} from "react-router-dom";
 import {Home} from "./pages/Home.jsx"
 import {AllCountries} from "./pages/AllCountries.jsx";
 import Country from "./pages/Country.jsx";
-import LogIn from "./pages/LogIn.jsx";
+import LogIn from "./pages/login.jsx";
 import SignUp from "./pages/signUp.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 function App() {
